96c235f4bddd7f0cb58e96e5374c2aaf502a935f
Clément Christiaens <clementchristiaens@pm.me>
2023-04-28 09:17:08 +0200

    Start

---
+++ src/main/java/org/projet/cypath/HelloController.java
@@ -0,0 +1,14 @@
+package org.projet.cypath;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
