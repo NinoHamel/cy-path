8ffe3b5fc8ab32ed0cb81777d3f3140dd733c279
Clément Christiaens <clementchristiaens@pm.me>
2023-05-15 20:25:35 +0000

    Upload New File

---
+++ src/main/java/org/projet/cypath/MainGame.java
@@ -0,0 +1,68 @@
+package org.projet.cypath;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainGame extends Application {
+    private Stage primaryStage;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        this.primaryStage = stage;
+        showStartScene();
+    }
+
+    public void showStartScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("start_scene.fxml"));
+        Parent root = loader.load();
+        StartSceneController controller = loader.getController();
+        controller.setMainGame(this);
+        primaryStage.setTitle("Game Start");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinWidth(1720);
+        primaryStage.setMinHeight(720);
+        primaryStage.show();
+    }
+
+
+    public void showGameScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("game_scene.fxml"));
+        Parent root = loader.load();
+        GameSceneController controller = loader.getController();
+        controller.setMainGame(this);
+        primaryStage.setTitle("Game");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public void showEndScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("end_scene.fxml"));
+        Parent root = loader.load();
+        EndSceneController controller = loader.getController();
+        controller.setMainGame(this);
+        primaryStage.setTitle("Game Over");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public void showSaveLoadScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("save_load_scene.fxml"));
+        Parent root = loader.load();
+        SaveLoadSceneController controller = loader.getController();
+        controller.setMainGame(this);
+        primaryStage.setTitle("Save & Load");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
