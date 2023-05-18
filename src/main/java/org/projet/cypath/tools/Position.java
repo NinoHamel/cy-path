4c4912aabef60cc62fa11214eee238944605994d
Côme QUINTYN <quintyn.c@hotmail.com>
2023-05-18 13:56:39 +0000

    Correction des indices de position pour qu'il aillent de 0 à 8 et non de 1 à 9

--- src/main/java/org/projet/cypath/tools/Position.java
+++ src/main/java/org/projet/cypath/tools/Position.java
@@ -21,8 +21,8 @@ public class Position{
      * @throws OutOfBoardException the exception when a position is out of the board
      */
     public Position(int abscissa, int ordinate) throws OutOfBoardException{
-        if(abscissa <= 0 || ordinate <= 0 ) throw new OutOfBoardException("The grid has only positive coordinates.");
-        if(abscissa > 9 || ordinate > 9 ) throw new OutOfBoardException("The grid has only coordinates greater than 9.");
+        if(abscissa < 0 || ordinate < 0 ) throw new OutOfBoardException("The grid has only positive coordinates.");
+        if(abscissa >= 9 || ordinate >= 9 ) throw new OutOfBoardException("The grid has only coordinates smaller than 9.");
         this.abscissa = abscissa;
         this.ordinate = ordinate;
     }
@@ -59,8 +59,8 @@ public class Position{
      */
     public void move(int abscissaShift, int ordinateShift) throws OutOfBoardException {
         // TODO Remplacer la taille min et la taille max de la grille 0 et 9 par une constante de la classe Board
-        if(this.abscissa + abscissaShift <= 0 || this.ordinate + ordinateShift <= 0 ) throw new OutOfBoardException("The grid has only positive coordinates.");
-        if(this.abscissa + abscissaShift > 9 || this.ordinate + ordinateShift > 9 ) throw new OutOfBoardException("The grid has only coordinates greater than 9.");
+        if(this.abscissa + abscissaShift < 0 || this.ordinate + ordinateShift < 0 ) throw new OutOfBoardException("The grid has only positive coordinates.");
+        if(this.abscissa + abscissaShift >= 9 || this.ordinate + ordinateShift >= 9 ) throw new OutOfBoardException("The grid has only coordinates greater than 9.");
         this.abscissa += abscissaShift;
         this.ordinate += ordinateShift;
     }
