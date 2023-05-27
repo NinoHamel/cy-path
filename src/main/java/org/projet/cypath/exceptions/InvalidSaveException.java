4699dbd7f8a9dc5e3453fd055d977f796ed019df
Clement Christiaens <clement.christiaens@etu.cyu.fr>
2023-05-27 23:56:39 +0000

    InvalideSaveException

---
+++ src/main/java/org/projet/cypath/exceptions/InvalidSaveException.java
@@ -0,0 +1,19 @@
+package org.projet.cypath.exceptions;
+
+
+/**
+ * Class which represent the exception when a game instance does not exist
+ *
+ * @author Clément CHRISTIAENS
+ * @version 1.0
+ */
+public class InvalidSaveException extends Exception{
+
+    /**
+     * Create an InvalidSaveException
+     */
+    public InvalidSaveException() {
+        System.out.println("New InvalidSceneException :");
+        System.out.println("You can't save a menu :/");
+    }
+}
