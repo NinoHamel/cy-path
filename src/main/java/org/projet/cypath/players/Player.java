a6fd0236ba18e74e6323989a1e10a8873cb34c89
Lucas <lucas.velay2@gmail.com>
2023-05-28 16:32:26 +0200

    Commit resolve warnings

--- src/main/java/org/projet/cypath/players/Player.java
+++ src/main/java/org/projet/cypath/players/Player.java
@@ -108,14 +108,15 @@ public class Player implements Serializable {
     }
     /**
      * Setter of victory
-     * @param victory
+     * @param victory the boolean that represent player victory
      */
     public void setVictory(boolean victory){
         this.victory=victory;
     }
+
     /**
      * Getter of victory
-     * @return
+     * @return a boolean indicating the player victory
      */
     public boolean isVictory(){
         return this.victory;
@@ -157,18 +158,6 @@ public class Player implements Serializable {
     }
 
     /**
-     * Move the player by changing his position
-     * @param newRow the new row of the player
-     * @param newColumn the new column of the player
-     * @param board is the board of the game
-     * @throws OutOfBoardException the exception when a position is out of the board
-     */
-    public void moveTo(int newRow, int newColumn, Board board) throws OutOfBoardException {
-        this.currentBox.setHasPlayer(false);
-        this.currentBox = board.getBox(newRow, newColumn);
-        this.currentBox.setHasPlayer(true);
-    }
-    /**
      *
      * Move the player by changing his position
      * @param newBox the new box of the player
@@ -183,7 +172,7 @@ public class Player implements Serializable {
      *Give a list of box, with every possible move for a player
      * @param board is the board of the game
      * @return possibleMove a list of box where the player can move to
-     * @throws OutOfBoardException
+     * @throws OutOfBoardException the exception when a position is out of the board
      */
     public List<Box> possibleMove(Board board) throws OutOfBoardException {
         List<Box> possibleMove = new ArrayList<>();
@@ -219,7 +208,7 @@ public class Player implements Serializable {
             }
         }
 
-        // Déplacement droite
+        // Move right
         if (board.onBoard(rowPlayer, columnPlayer + 1)) {
             int rowRight = rowPlayer;
             int columnRight = columnPlayer + 1;
@@ -251,117 +240,115 @@ public class Player implements Serializable {
     }
 
     /**
-     * This method is used in {@link #possibleMove(Board)} to give differents move possible if there is 2 players next to each other.
+     * This method is used in {@link #possibleMove(Board)} to give different move possible if there is 2 players next to each other.
      * @param board is the board where the player moves
      * @param newBox is the box where another player is
-     * @param possibleMove is
-     * @throws OutOfBoardException
+     * @param possibleMove is the list of the possible move
+     * @throws OutOfBoardException the exception when a position is out of the board
      */
     public void possibleMoveJump(Board board, Box newBox, List<Box> possibleMove, String Orientation) throws OutOfBoardException {
-        int rowPlayer = this.getCurrentBox().getRow();
-        int columnPlayer = this.getCurrentBox().getColumn();
         Box newNewBox=null;
-        if (Orientation=="Right" && board.onBoard(newBox.getRow(), newBox.getColumn()+1)) {
+        if (Orientation.equals("Right") && board.onBoard(newBox.getRow(), newBox.getColumn()+1)) {
                     newNewBox = board.getBox(newBox.getRow(), newBox.getColumn() + 1);
         }
-        if (Orientation=="Left" && board.onBoard(newBox.getRow(), newBox.getColumn()-1)) {
+        if (Orientation.equals("Left") && board.onBoard(newBox.getRow(), newBox.getColumn()-1)) {
                     newNewBox = board.getBox(newBox.getRow(), newBox.getColumn() - 1);
         }
-        if (Orientation=="Top" && board.onBoard(newBox.getRow()-1, newBox.getColumn())) {
+        if (Orientation.equals("Top") && board.onBoard(newBox.getRow()-1, newBox.getColumn())) {
                     newNewBox = board.getBox(newBox.getRow()-1, newBox.getColumn());
         }
-        if (Orientation=="Bottom" && board.onBoard(newBox.getRow()+1, newBox.getColumn())) {
+        if (Orientation.equals("Bottom") && board.onBoard(newBox.getRow()+1, newBox.getColumn())) {
                     newNewBox = board.getBox(newBox.getRow()+1, newBox.getColumn());
         }
         if(
-                Orientation=="Right"&& (newBox.hasRightWall())||
-                Orientation=="Left"&& newBox.hasLeftWall()||
-                Orientation=="Top"&& newBox.hasTopWall()||
-                Orientation=="Bottom"&& newBox.hasBottomWall()||
+                Orientation.equals("Right") && (newBox.hasRightWall())||
+                Orientation.equals("Left") && newBox.hasLeftWall()||
+                Orientation.equals("Top") && newBox.hasTopWall()||
+                Orientation.equals("Bottom") && newBox.hasBottomWall()||
                 newNewBox!=null && newNewBox.hasPlayer() && !newNewBox.equals(this.getCurrentBox())
         )
      {
-            // Déplacement bas
+            //  Move bottom
             if (board.onBoard(newBox.getRow() + 1, newBox.getColumn())) {
                 int newRowBottom = newBox.getRow() + 1;
                 int newColumnBottom = newBox.getColumn();
                 Box newBoxBottom = board.getBox(newRowBottom, newColumnBottom);
-                if (board.onBoard(newRowBottom, newColumnBottom) && !newBox.hasBottomWall() && !newBoxBottom.equals(this.getCurrentBox()) && newBoxBottom.hasPlayer()==false) {
+                if (board.onBoard(newRowBottom, newColumnBottom) && !newBox.hasBottomWall() && !newBoxBottom.equals(this.getCurrentBox()) && !newBoxBottom.hasPlayer()) {
                     possibleMove.add(newBoxBottom);
                 }
             }
-            // Déplacement haut
+            // Move top
             if (board.onBoard(newBox.getRow() - 1, newBox.getColumn())) {
                 int newRowTop = newBox.getRow() - 1;
                 int newColumnTop = newBox.getColumn();
                 Box newBoxTop = board.getBox(newRowTop, newColumnTop);
-                if (board.onBoard(newRowTop, newColumnTop) && !newBox.hasTopWall() && !newBoxTop.equals(this.getCurrentBox()) && newBoxTop.hasPlayer()==false) {
+                if (board.onBoard(newRowTop, newColumnTop) && !newBox.hasTopWall() && !newBoxTop.equals(this.getCurrentBox()) && !newBoxTop.hasPlayer()) {
                     possibleMove.add(newBoxTop);
                 }
             }
-            // Déplacement droite
+            // Move left
             if (board.onBoard(newBox.getRow(), newBox.getColumn() + 1)) {
                 int newRowRight = newBox.getRow();
                 int newColumnRight = newBox.getColumn() + 1;
                 Box newBoxRight = board.getBox(newRowRight, newColumnRight);
-                if (board.onBoard(newRowRight, newColumnRight) && !newBox.hasRightWall() && !newBoxRight.equals(this.getCurrentBox()) && newBoxRight.hasPlayer()==false) {
+                if (board.onBoard(newRowRight, newColumnRight) && !newBox.hasRightWall() && !newBoxRight.equals(this.getCurrentBox()) && !newBoxRight.hasPlayer()) {
                     possibleMove.add(newBoxRight);
                 }
             }
 
-            // Déplacement gauche
+            // Move left
             if (board.onBoard(newBox.getRow(), newBox.getColumn() - 1)) {
                 int newRowLeft = newBox.getRow();
                 int newColumnLeft = newBox.getColumn() - 1;
                 Box newBoxLeft = board.getBox(newRowLeft, newColumnLeft);
-                if (board.onBoard(newRowLeft, newColumnLeft) && !newBox.hasLeftWall() && !newBoxLeft.equals(this.getCurrentBox()) && newBoxLeft.hasPlayer()==false) {
+                if (board.onBoard(newRowLeft, newColumnLeft) && !newBox.hasLeftWall() && !newBoxLeft.equals(this.getCurrentBox()) && !newBoxLeft.hasPlayer()) {
                     possibleMove.add(newBoxLeft);
                 }
             }
         }
         else{
-            //Déplacement Bas
-            if(Orientation == "Bottom"){
+            // Move bottom
+            if(Orientation.equals("Bottom")){
                 int newRowBottom = newBox.getRow()+1;
                 int newColumnBottom = newBox.getColumn();
                 if (board.onBoard(newRowBottom, newColumnBottom)) {
                     Box newBoxBottom = board.getBox(newRowBottom, newColumnBottom);
-                    if(newBoxBottom.hasPlayer()==false) {
+                    if(!newBoxBottom.hasPlayer()) {
                         possibleMove.add(newBoxBottom);
                     }
                 }
             }
-            //Déplacement Haut
-            if(Orientation == "Top"){
+            // Move top
+            if(Orientation.equals("Top")){
                 int newRowTop = newBox.getRow()-1;
                 int newColumnTop = newBox.getColumn();
 
                 if (board.onBoard(newRowTop, newColumnTop)) {
                     Box newBoxTop = board.getBox(newRowTop, newColumnTop);
-                    if (newBoxTop.hasPlayer()==false) {
+                    if (!newBoxTop.hasPlayer()) {
                         possibleMove.add(newBoxTop);
                     }
                 }
             }
-            //Déplacement Droit
-            if(Orientation == "Right"){
+            // Move right
+            if(Orientation.equals("Right")){
                 int newRowRight = newBox.getRow();
                 int newColumnRight = newBox.getColumn() + 1;
 
                 if (board.onBoard(newRowRight, newColumnRight)) {
                     Box newBoxRight = board.getBox(newRowRight, newColumnRight);
-                    if(newBoxRight.hasPlayer()==false) {
+                    if(!newBoxRight.hasPlayer()) {
                         possibleMove.add(newBoxRight);
                     }
                 }
             }
-            //Déplacement Gauche
-            if(Orientation == "Left"){
+            // Move left
+            if(Orientation.equals("Left")){
                 int newRowLeft = newBox.getRow();
                 int newColumnLeft = newBox.getColumn() - 1;
                 if (board.onBoard(newRowLeft, newColumnLeft)) {
                     Box newBoxLeft = board.getBox(newRowLeft, newColumnLeft);
-                    if(newBoxLeft.hasPlayer()==false) {
+                    if(!newBoxLeft.hasPlayer()) {
                         possibleMove.add(newBoxLeft);
                     }
                 }
@@ -371,7 +358,7 @@ public class Player implements Serializable {
 
     /**
      * Setter of the box where the player is
-     * @param box
+     * @param box the box where the player is
      */
     public void setCurrentBox(Box box) {
         this.currentBox=box;
